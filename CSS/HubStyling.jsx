8012398e8cd63b5bf635e14875e4f6bf108c2010
Collin Myers <collin_myers@outlook.com>
2023-10-27 10:25:24 -0400

    Fixed the stuff quince did

--- CSS/HubStyling.jsx
+++ CSS/HubStyling.jsx
@@ -1,47 +1,61 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FCF6EE",
-  },
-  textInput: {
-    width: "75%",
-    height: 400,
-    backgroundColor: "#f6e3cb",
-    marginBottom: 16,
-    color: "red",
-    padding: 10,
-  },
-  journalInput: {
-    width: "75%",
-    height: 400,
-    backgroundColor: "#f6e3cb",
-    margin: 16,
-    padding: 10,
-    borderRadius: 15,
-  },
-  titleText: {
-    fontSize: 48,
-    color: "#21080C",
-    fontWeight: "bold",
-    margin: 24,
-  },
-  buttonOpac: {
-    backgroundColor: "#21080C",
-    borderRadius: 5,
-    marginTop: 10,
-    marginBottom: 20,
-    paddingLeft: 30,
-    paddingRight: 30,
-    paddingTop: 10,
-    paddingBottom: 10,
-    textAlign: "center",
-  },
-  actionButtonText: {
-    color: "#21080F",
-  },
- 
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FCF6EE",
+    },
+    textInput: {
+        width: "75%",
+        height: 400,
+        backgroundColor: "#f6e3cb",
+        marginBottom: 16,
+        color: "red",
+        padding: 10,
+    },
+    journalInput: {
+        width: "75%",
+        height: 400,
+        backgroundColor: "#f6e3cb",
+        margin: 16,
+        padding: 10,
+        borderRadius: 15,
+    },
+    titleText: {
+        fontSize: 48,
+        color: "#21080C",
+        fontWeight: "bold",
+        margin: 24,
+    },
+    buttonOpac: {
+        backgroundColor: "#21080C",
+        borderRadius: 5,
+        marginTop: 10,
+        marginBottom: 20,
+        paddingLeft: 30,
+        paddingRight: 30,
+        paddingTop: 10,
+        paddingBottom: 10,
+        textAlign: "center",
+    },
+    actionButtonText: {
+        color: "#21080F",
+    },
+    subText: {
+        textDecorationLine: "underline",
+        color: "#EE4B2B",
+    },
+    exerciseText: {
+        textAlign: "center",
+        fontWeight: "bold",
+        color: "brown",
+        padding: 10,
+    },
+    exerciseTitle: {
+        fontSize: 36,
+        fontWeight: "bold"
+    }
+
 });
--- screens/hub/Exercise.jsx
+++ screens/hub/Exercise.jsx
@@ -4,23 +4,23 @@ import { SafeAreaView } from 'react-native';
 import { Text } from 'react-native-paper';
 
 // import Theme from '../../CSS/AppTheme';
-import Hub from '../../CSS/ExerStyling';
+import Hub from '../../CSS/HubStyling';
 
 export default function ExercisePage() {
 
     return (
-      <SafeAreaView style={Hub.container}>
-        <Text style={Hub.titleText}>Wellness Exercises</Text>
-        <Text style={Hub.exerciseText}>
-          Here are some mental health techniques to improve your emotional
-          well-being, reduce stress and induce relaxation:
-        </Text>
+        <SafeAreaView style={Hub.container}>
+            <Text style={Hub.exerciseTitle}>Wellness Exercises</Text>
+            <Text style={Hub.exerciseText}>
+                Here are some mental health techniques to improve your emotional
+                well-being, reduce stress and induce relaxation:
+            </Text>
 
-        <Text style={Hub.subText}>4-7-8 Breathing</Text>
-        <Text style={Hub.subText}>Diaphragmatic Breathing</Text>
-        <Text style={Hub.subText}>Progressive Muscle Relaxation</Text>
-        <Text style={Hub.subText}>Positive Affirmations</Text>
-        <Text style={Hub.subText}>Mindfulness Meditation</Text>
-      </SafeAreaView>
+            <Text style={Hub.subText}>4-7-8 Breathing</Text>
+            <Text style={Hub.subText}>Diaphragmatic Breathing</Text>
+            <Text style={Hub.subText}>Progressive Muscle Relaxation</Text>
+            <Text style={Hub.subText}>Positive Affirmations</Text>
+            <Text style={Hub.subText}>Mindfulness Meditation</Text>
+        </SafeAreaView>
     );
 }
