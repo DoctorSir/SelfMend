1c4fa5151048fcb5f4f446b2c5eaeb3414cfd75a
Collin Myers <collin_myers@outlook.com>
2023-11-02 21:43:01 -0400

    Updated firebase config for db and push notification capabilities

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getMessaging } from "firebase/messaging/sw";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -15,4 +17,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const messaging = getMessaging(firebaseApp);
