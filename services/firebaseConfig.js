e231d876cfb6a133f990f0441ccd74a0a9ecd5a4
Collin Myers <collin_myers@outlook.com>
2023-11-03 00:16:26 -0400

    changed firebase config

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getMessaging } from "firebase/messaging/sw";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,7 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const messaging = getMessaging(firebaseApp);
+
+
