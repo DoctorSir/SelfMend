8d672c82077c3e14de806604b15f7dc168e7df54
Quincy Nguyen <qwn5005@psu.edu>
2023-11-03 21:16:26 -0400

    More exercise stuff

--- CSS/HubStyling.jsx
+++ CSS/HubStyling.jsx
@@ -1,101 +1,123 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#FCF6EE",
-    },
-    textInput: {
-        width: "75%",
-        height: 400,
-        backgroundColor: "#F6E3CB",
-        marginBottom: 16,
-        color: "red",
-        padding: 10,
-    },
-    journalInput: {
-        width: "75%",
-        height: 400,
-        backgroundColor: "#F6E3CB",
-        margin: 16,
-        padding: 5,
-    },
-    journalOutline: {
-        borderWidth: 0,
-        borderRadius: 25,
-    },
-    titleText: {
-        fontSize: 42,
-        color: "#21080C",
-        fontWeight: "bold",
-        margin: 24,
-    },
-    headerText: {
-        fontSize: 16,
-        color: '#5194B8',
-    },
-    buttonOpac: {
-        backgroundColor: "#21080C",
-        borderRadius: 5,
-        marginTop: 10,
-        marginBottom: 20,
-        paddingLeft: 30,
-        paddingRight: 30,
-        paddingTop: 10,
-        paddingBottom: 10,
-        textAlign: "center"
-    },
-    submitOpac: {
-        backgroundColor: "#70B533",
-        borderRadius: 5,
-        marginTop: 10,
-        marginBottom: 20,
-        paddingLeft: 30,
-        paddingRight: 30,
-        paddingTop: 10,
-        paddingBottom: 10,
-        textAlign: "center"
-    },
-    actionButtonText: {
-        color: "#FCF6EE",
-    },
-    dropdown: {
-        borderWidth: 2,
-        borderRadius: 20,
-        width: "50%",
-        marginTop: 10,
-        marginBottom: 30,
-        height: 50,
-        backgroundColor: '#FCF6EE'
-    },
-    subText: {
-        textDecorationLine: "underline",
-        color: "#70B533",
-    },
-    exerciseText: {
-        textAlign: "center",
-        fontWeight: "bold",
-        color: "#21080C",
-        padding: 10,
-    },
-    exerciseTitle: {
-        fontSize: 36,
-        fontWeight: "bold",
-        color: "#5194B8"
-    },
-    welcomeText: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginVertical: 25,
-    },
-    reminderText: {
-        fontSize: 18,
-        color: '#555555',
-        textAlign: 'center',
-        paddingHorizontal: 20,
-        marginVertical: 25
-    },
-
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F6E3CB",
+  },
+  textInput: {
+    width: "75%",
+    height: 400,
+    backgroundColor: "#F6E3CB",
+    marginBottom: 16,
+    color: "red",
+    padding: 10,
+  },
+  journalInput: {
+    width: "75%",
+    height: 400,
+    backgroundColor: "#F6E3CB",
+    margin: 16,
+    padding: 5,
+  },
+  journalOutline: {
+    borderWidth: 0,
+    borderRadius: 25,
+  },
+  titleText: {
+    fontSize: 42,
+    color: "#21080C",
+    fontWeight: "bold",
+    margin: 24,
+  },
+  headerText: {
+    fontSize: 16,
+    color: "#5194B8",
+  },
+  buttonOpac: {
+    backgroundColor: "#21080C",
+    borderRadius: 5,
+    marginTop: 10,
+    marginBottom: 20,
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 10,
+    paddingBottom: 10,
+    textAlign: "center",
+  },
+  submitOpac: {
+    backgroundColor: "#70B533",
+    borderRadius: 5,
+    marginTop: 10,
+    marginBottom: 20,
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 10,
+    paddingBottom: 10,
+    textAlign: "center",
+  },
+  actionButtonText: {
+    color: "#FCF6EE",
+  },
+  dropdown: {
+    borderWidth: 2,
+    borderRadius: 20,
+    width: "50%",
+    marginTop: 10,
+    marginBottom: 30,
+    height: 50,
+    backgroundColor: "#FCF6EE",
+  },
+  subText: {
+    textDecorationLine: "underline",
+    color: "#70B533",
+  },
+  exerciseText: {
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "#21080C",
+    padding: 10,
+  },
+  exerciseTitle: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#21080C",
+    textAlign: "center",
+    padding: 32,
+  },
+  button: {
+    padding: 5,
+    borderRadius: 5,
+  },
+  buttonBackground: {
+    backgroundColor: "#090909",
+  },
+  buttonBackgroundPressed: {
+    backgroundColor: "#FCF6EE",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    padding: 1,
+  },
+  buttonTextPressed: {
+    textAlign: "center",
+    color: "black",
+    padding: 20,
+    fontSize: 18,
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginVertical: 25,
+  },
+  reminderText: {
+    fontSize: 18,
+    color: "#555555",
+    textAlign: "center",
+    paddingHorizontal: 20,
+    marginVertical: 25,
+  },
 });
--- screens/hub/Exercise.jsx
+++ screens/hub/Exercise.jsx
@@ -1,26 +1,141 @@
+import React, { useState } from "react";
+import { View, Text, Pressable, ScrollView } from "react-native";
+import Hub from "../../CSS/HubStyling"; // Import your styling
 
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { Text } from 'react-native-paper';
-
-// import Theme from '../../CSS/AppTheme';
-import Hub from '../../CSS/HubStyling';
+const accordionItems = [
+  {
+    title: "Diaphragmatic Breathing Steps",
+    content: [
+      "Step 1. Find a Comfortable Position: Start by finding a quiet and comfortable place to sit or lie down. You can also do diaphragmatic breathing while standing, but it's often easier to learn while sitting or lying down.",
+      "Step 2. Relax Your Body: Make sure your shoulders are relaxed and not hunched. You can close your eyes or keep them open, depending on your preference. Relax your facial muscles, jaw, and neck.",
+      "Step 3. Place Your Hand on Your Abdomen: You can place one hand on your abdomen just below your ribcage and the other on your chest. This will help you feel the movement of your diaphragm and chest.",
+      "Step 4. Inhale Slowly Through Your Nose: Breathe in slowly and deeply through your nose. Focus on filling your lungs from the bottom up. As you do this, you should feel your abdomen rise as your diaphragm contracts and pushes downward. Try to keep your chest relatively still during this phase.",
+      "Step 5. Exhale Slowly Through Your Mouth: Exhale slowly and completely through your mouth. As you do this, you should feel your abdomen fall as your diaphragm relaxes and moves upward.",
+      "Step 6. Continue the Breathing Cycle: Continue this deep breathing pattern, focusing on the rise and fall of your abdomen. Inhale slowly for a count of 4-6 seconds and exhale for a similar count. The exact timing isn't as important as making your breaths slow and controlled.",
+      "Step 7. Practice Regularly: Practice diaphragmatic breathing for at least a few minutes each day. You can gradually increase the duration of your practice as you become more comfortable with this technique.",
+      "Step 8. Pay Attention to Your Breath: As you practice, pay attention to your breath and the sensations in your body. Try to let go of any distracting thoughts and focus on your breath to help reduce stress and increase relaxation.",
+      "Step 9. Use Diaphragmatic Breathing in Stressful Situations: Once you've become proficient at diaphragmatic breathing, you can use it to manage stress, anxiety, or other intense emotions. Whenever you feel stressed or anxious, take a moment to practice deep breathing.",
+      "Remember that diaphragmatic breathing is a skill that may take some time to master. With regular practice, it can become a valuable tool for managing stress and promoting relaxation in your daily life.",
+    ],
+  },
+  {
+    title: "4-7-8 Breathing Steps",
+    content: [
+      "Step 1: Find a quiet place, sit or lie in a comfortable position, and close your eyes.",
+      "Step 2: Take a deep breathe: Inhale quietly and deeply for 4 seconds.",
+      "Step 3: Hold your breathe: Hold your breathe for a count of 7 seconds.",
+      "Step 4: Exhale slowly: Exhale slowly through your mouth for a count of 8 seconds.",
+      "Step 5: Repeat the cycle, maintain a rhythm, and practice regularly.",
+    ],
+  },
+  {
+    title: "Progressive Muscle Relaxation Steps",
+    content: [
+      "Progressive Muscle Relaxation (PMR) is a relaxation technique that involves tensing and then relaxing different muscle groups in your body to reduce physical tension and promote relaxation. It can be an effective way to manage stress and anxiety",
+    ],
+  },
+  {
+    title: "Press me to discover something fun!",
+    content: [
+      "Just kidding. No fun for you yet.",
+    ]
+  },
+  {
+    title: "Metallica - Nothing Else Matters",
+    content: [
+      "So close, no matter how far",
+"Couldn't be much more from the heart",
+"Forever trusting who we are",
+"And nothing else matters",
+"Never opened myself this way",
+"Life is ours, we live it our way",
+"All these words, I don't just say",
+"And nothing else matters",
+"Trust I seek and I find in you",
+"Every day for us something new",
+"Open mind for a different view",
+"And nothing else matters",
+"Never cared for what they do",
+"Never cared for what they know",
+"But I know",
+"So close, no matter how far",
+"It couldn't be much more from the heart",
+"Forever trusting who we are",
+"And nothing else matters",
+"Never cared for what they do",
+"Never cared for what they know",
+"But I know",
+"I never opened myself this way",
+"Life is ours, we live it our way",
+"All these words, I don't just say",
+"And nothing else matters",
+"Trust I seek and I find in you",
+"Every day for us something new",
+"Open mind for a different view",
+"And nothing else matters",
+"Never cared for what they say",
+"Never cared for games they play",
+"Never cared for what they do",
+"Never cared for what they know",
+"And I know, yeah, yeah",
+"So close, no matter how far",
+"Couldn't be much more from the heart",
+"Forever trusting who we are",
+"No, nothing else matters"
+    ]
+  }
+  // Add more accordion items as needed
+];
 
 export default function ExercisePage() {
+  const [expandedIndex, setExpandedIndex] = useState(null);
 
-    return (
-        <SafeAreaView style={Hub.container}>
-            <Text style={Hub.exerciseTitle}>Wellness Exercises</Text>
-            <Text style={Hub.exerciseText}>
-                Here are some mental health techniques to improve your emotional
-                well-being, reduce stress and induce relaxation:
-            </Text>
+  const toggleAccordion = (index) => {
+    setExpandedIndex(index === expandedIndex ? null : index);
+  };
 
-            <Text style={Hub.subText}>4-7-8 Breathing</Text>
-            <Text style={Hub.subText}>Diaphragmatic Breathing</Text>
-            <Text style={Hub.subText}>Progressive Muscle Relaxation</Text>
-            <Text style={Hub.subText}>Positive Affirmations</Text>
-            <Text style={Hub.subText}>Mindfulness Meditation</Text>
-        </SafeAreaView>
-    );
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 80 }}>
+      <ScrollView showsVerticalScrollIndicator={false} style={{ width: "120%"}}>
+        <Text style={Hub.exerciseTitle}>Mental Health Exercises</Text>
+        {accordionItems.map((item, index) => (
+          <View key={index}>
+            <Pressable
+              style={({ pressed }) => [
+                Hub.button,
+                expandedIndex === index
+                  ? Hub.buttonBackgroundPressed
+                  : Hub.buttonBackground,
+                pressed
+                  ? { borderRadius: 10, marginBottom: 10 }
+                  : { borderRadius: 10, marginBottom: 10 },
+              ]}
+              onPress={() => toggleAccordion(index)}
+            >
+              <Text
+                style={[
+                  Hub.buttonText,
+                  expandedIndex === index ? Hub.buttonTextPressed : null,
+                ]}
+              >
+                {item.title}
+              </Text>
+            </Pressable>
+            {expandedIndex === index && (
+              <View
+                style={[
+                  Hub.exerciseAccordionContent,
+                  { marginTop: 5, padding: 10 },
+                ]}
+              >
+                {item.content.map((step, stepIndex) => (
+                  <Text key={stepIndex}>{step}</Text>
+                ))}
+              </View>
+            )}
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
 }
