d9e17f3611f6f3d65d838305c92d3590ec5fc1cc
Quincy Nguyen <qwn5005@psu.edu>
2023-11-10 16:57:53 -0500

    Small exercise updates b4 pulling

--- CSS/HubStyling.jsx
+++ CSS/HubStyling.jsx
@@ -113,7 +113,7 @@ export default StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    padding: 80,
+    padding: 50,
     backgroundColor: "#FCF6EE"
   },
   welcomeText: {
